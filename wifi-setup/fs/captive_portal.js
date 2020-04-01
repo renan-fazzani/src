1d1dc6cb3b60d18589303b7e6f951bf317a05d7b
Renan Fazzani <renan.alexandre@cerebrasc.com.br>
2020-04-01 16:15:54 -0300

    Update captive_portal.js

--- wifi-setup/fs/captive_portal.js
+++ wifi-setup/fs/captive_portal.js
@@ -86,7 +86,7 @@
                 responseVal = "Unable to get info from device!";
                 //document.getElementById("response").innerHTML = responseVal ? responseVal : '';
             }
-            document.getElementById("response").innerHTML = "ssid: " + resp.wifi.sta.ssid + "<br>" + "pass: "resp.wifi.sta.pass; 
+            document.getElementById("response").innerHTML = "ssid: " + resp.wifi.sta.ssid + "<br>" + "pass: " + resp.wifi.sta.pass; 
             //document.getElementById("response").innerHTML = responseVal ? responseVal : '';
             // Need to check if function since this is called by event handler
             if ( typeof callback === "function" ) {
